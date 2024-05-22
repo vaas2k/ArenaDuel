494d473e99b2b8ae694cb84a57f726d6b71a36a1
vaas2k <sk315425@gmail.com>
2024-05-22 12:40:31 +0500

    noti issue 5.3 fixed

--- app/(root)/about/page.tsx
+++ app/(root)/about/page.tsx
@@ -1,10 +1,12 @@
-import Notifications from '@/components/shared/Notifications'
+const Notifications = dynamic(() => import("@/components/shared/Notifications"), { ssr: false });
+
+import dynamic from 'next/dynamic';
 import React from 'react'
 
 const About = () => {
   return (
     <div className='flex items-center justify-center'>
-        <Notifications />
+      <Notifications />
     </div>
   )
 }
