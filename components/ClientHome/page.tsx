164988971c254eee2f102d39e2039eb9d57fb527
vaas2k <sk315425@gmail.com>
2024-05-22 12:32:26 +0500

    noti issue 5.2 fixed

--- components/ClientHome/page.tsx
+++ components/ClientHome/page.tsx
@@ -1,21 +1,26 @@
 "use client";
 
-import CalltoAction from "@/components/LandingPage/CalltoAction";
-import Clients from "@/components/LandingPage/Clients";
-import Commitment from "@/components/LandingPage/Commitment";
-import Community from "@/components/LandingPage/Community";
-import Discover from "@/components/LandingPage/Discover";
-import Footer from "@/components/LandingPage/Footer";
-import Hero from "@/components/LandingPage/Hero";
-import { Loader } from "@/components/shared/Loader";
-import axios from "axios";
-import { useSession } from "next-auth/react";
+import dynamic from "next/dynamic";
+import React, { useEffect, useState } from "react";
 import { Rubik } from "next/font/google";
-import { useEffect, useState } from "react";
+import { useSession } from "next-auth/react";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { Loader } from "@/components/shared/Loader";
+
+// Dynamically import components that may access the DOM
+const CalltoAction = dynamic(() => import("@/components/LandingPage/CalltoAction"), { ssr: false });
+const Clients = dynamic(() => import("@/components/LandingPage/Clients"), { ssr: false });
+const Commitment = dynamic(() => import("@/components/LandingPage/Commitment"), { ssr: false });
+const Community = dynamic(() => import("@/components/LandingPage/Community"), { ssr: false });
+const Discover = dynamic(() => import("@/components/LandingPage/Discover"), { ssr: false });
+const Footer = dynamic(() => import("@/components/LandingPage/Footer"), { ssr: false });
+const Hero = dynamic(() => import("@/components/LandingPage/Hero"), { ssr: false });
 
 const rubik = Rubik({ subsets: ["latin"] });
 
 export default function ClientHome() {
+  const router = useRouter();
   const [isMounted, setIsMounted] = useState(false);
   const { data: session, status } = useSession();
 
@@ -29,7 +34,7 @@ export default function ClientHome() {
         const { data } = await axios.get(`/api/profile/${email}`);
         if (data.status === 200) {
           if (typeof window !== "undefined") {
-            window.sessionStorage.setItem('user', JSON.stringify(data.data));
+            window.sessionStorage.setItem("user", JSON.stringify(data.data));
           }
           console.log(data.data);
         } else {
