12530577536e9803674afdeabd4516a6709486b2
vaas2k <sk315425@gmail.com>
2024-05-22 12:16:07 +0500

    noti issue 3 fixed

--- app/api/friends/addfriend/route.ts
+++ app/api/friends/addfriend/route.ts
@@ -2,50 +2,61 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export async function POST( req : Request, res : Response) {
-    
-    const data = await req.json();
+export async function POST(req: Request) {
+  const data = await req.json();
 
-    try{
+  try {
+    const getUser = await prisma.user.findFirst({
+      where: {
+        email: data.email,
+      },
+    });
 
-        const getUser = await prisma.user.findFirst({
-            where : {
-                email : data.email
-            }
-        })
-
-
-        await prisma.friends.create({
-          data: {
-            userID : getUser?.id,
-            sender_name: data.sender_name,
-            sender_email: data.sender_email,
-            sender_image: data.sender_image,
-            accepted: false,
-          }
-        });
-        
-        await prisma.notifications.create({
-            data : {
-                userID : getUser?.id,
-                sender_name: data.sender_name,
-                sender_email: data.sender_email,
-                sender_image: data.sender_image,
-                message : "Friend Request",
-            }
-        })
-
-
-        return new Response(JSON.stringify({
-            msg : 'Good!',
-            status : 200
-        }))
-
-    }catch(error){
-        console.log(error);
-        return new Response(JSON.stringify({
-            msg : 'Internal Server Error',
-            status : 500
-        }))
+    if (!getUser || !getUser.id) {
+      return new Response(
+        JSON.stringify({
+          msg: 'User not found',
+          status: 404,
+        }),
+        { status: 404 }
+      );
     }
+
+    await prisma.friends.create({
+      data: {
+        userID: getUser.id,
+        sender_name: data.sender_name,
+        sender_email: data.sender_email,
+        sender_image: data.sender_image,
+        accepted: false,
+      },
+    });
+
+    await prisma.notifications.create({
+      data: {
+        userID: getUser.id,
+        sender_name: data.sender_name,
+        sender_email: data.sender_email,
+        sender_image: data.sender_image,
+        message: "Friend Request",
+      },
+    });
+
+    return new Response(
+      JSON.stringify({
+        msg: 'Good!',
+        status: 200,
+      }),
+      { status: 200 }
+    );
+  } catch (error) {
+    console.log(error);
+    return new Response(
+      JSON.stringify({
+        msg: 'Internal Server Error',
+        status: 500,
+      }),
+      { status: 500 }
+    );
+  }
 }
