b4b0624dd74570b6d6f08ee95840f870632864e5
vaas2k <sk315425@gmail.com>
2024-07-08 11:07:36 +0500

    useless page deleted

--- app/(root)/solution/[id]/page.tsx
+++ app/(root)/solution/[id]/page.tsx
@@ -1,32 +1,8 @@
-'use client'
-import { Editor } from '@monaco-editor/react'
-import { Badge, Card } from '@radix-ui/themes'
-import { X } from 'lucide-react'
-import { useEffect, useState } from 'react'
 import { Rubik } from "next/font/google";
 import WinningCard from '@/components/Dashboard/WinningCard'
-const rubik = Rubik({ subsets: ["latin"] });
 
-const page = ({params} : any) => {
+const Page = () => {
 
-  const id = params.id
-  const [code , setCode] = useState('');
-  const [showSolution , setSolution ] = useState(false);
-
-  useEffect(() => {
-
-    (async function () {
-
-      // get solution
-      try{
-
-      }catch(error) {
-        console.log(error);
-      }
-
-     })();
-
-  },[params]);
 
   return (
     <>
@@ -35,4 +11,4 @@ const page = ({params} : any) => {
   )
 }
 
-export default page
+export default Page
