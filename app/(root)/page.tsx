3552cf3aeebc223914ac9f3071ce798101308f0b
vaas2k <sk315425@gmail.com>
2024-07-16 12:35:21 +0500

    Full App

--- app/(root)/page.tsx
+++
@@ -1,7 +0,0 @@
-import dynamic from "next/dynamic";
-const ClientHome = dynamic(() => import("../../components/ClientHome/page"), { ssr: false })
-
-export default function Home() {
-  
-  return (<ClientHome />) ;
-}
